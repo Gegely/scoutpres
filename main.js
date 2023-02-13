8ec593cf2954d40bec03fe2db90dd5ddf222ec31
Gegely <mr.szeles.g@gmail.com>
2023-02-13 14:32:29 +0100

    link4

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 import './style.css'
 
 import * as THREE from 'https://unpkg.com/three@0.138.0/build/three.module.js';
+//import * as THREE from 'three';
 //import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 //import * as dat from "dat.gui";
 import { LineSegments, Vector3, WireframeGeometry } from "three";
