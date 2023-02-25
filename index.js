1864bbc8bece9174dd0702d954204f46fdd2382f
Gegely <mr.szeles.g@gmail.com>
2023-02-25 09:23:46 +0100

    Updates

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 //import './style.css'
 
-import * as THREE from 'https://unpkg.com/three@0.138.0/build/three.module.js';
-//import * as THREE from 'three';
+//import * as THREE from 'https://unpkg.com/three@0.138.0/build/three.module.js';
+import * as THREE from 'three';
 //import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 //import * as dat from "dat.gui";
 //import { LineSegments, Vector3, WireframeGeometry } from "https://unpkg.com/three@0.138.0/build/three.module.js";
-import { LineSegments, Vector3, WireframeGeometry } from "https://unpkg.com/three@0.138.0/build/three.module.js";
-//import { LineSegments, Vector3, WireframeGeometry } from "three";
+//import { LineSegments, Vector3, WireframeGeometry } from "https://unpkg.com/three@0.138.0/build/three.module.js";
+import { LineSegments, Vector3, WireframeGeometry } from "three";
 
-//import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
-import { GLTFLoader } from "https://unpkg.com/three@0.138.0/examples/jsm/loaders/GLTFLoader.js";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
+//import { GLTFLoader } from "https://unpkg.com/three@0.138.0/examples/jsm/loaders/GLTFLoader.js";
 //import { CSS3DRenderer, CSS3DObject } from 'three/addons/renderers/CSS3DRenderer.js';
 
 
